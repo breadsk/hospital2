8d8b87aa25597d8a767ad7f408c39676cd1394c9
Nicolas <nicolas.programador@gmail.com>
2025-11-04 19:01:57 -0300

    patient ok

---
+++ src/main/java/com/hospital_vm/cl/hospital_vm/model/Patient.java
@@ -0,0 +1,39 @@
+package com.hospital_vm.cl.hospital_vm.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "patient")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, length = 10, nullable = false)
+    private String rut;
+
+    @Column(nullable = false)
+    private String names;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Date birthDate;
+
+    @Column(nullable = false)
+    private String email;
+
+}
